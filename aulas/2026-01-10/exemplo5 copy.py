9ae91c8843629be1c6f0a1da61af9eff5350b75e
ericoborgonove <borgonove@gmail.com>
2026-01-10 11:41:49 -0400

    aula do dia 10/01/2026

---
+++ aulas/2026-01-10/exemplo5 copy.py
@@ -0,0 +1,17 @@
+pessoa1 = {
+    "nome": "Debora",
+    "idade": "37",
+    "profissão": "advogada"
+}
+
+pessoa2 = {
+    "nome": "Erico Borgonove",
+    "idade": "38",
+    "profissao": "Analista de Sistemas",
+    "graduação": "Mestrando",
+    "estado_civil": "Solteiro",
+    "nacionalidade": "Brasileiro"
+}
+
+print(pessoa1["nome"])
+print(pessoa2)
